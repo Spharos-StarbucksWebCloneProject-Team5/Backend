833a4546a8687854339da3cf4292d3dda8734003
jinyeong-afk <xoeh7002@naver.com>
2023-03-03 10:32:10 +0900

    ResposnePage vo 형태로 데이터 반환 타입 변환 pageNum, TotalPage, totalElements, pageSize, content(product 정보)

--- src/main/java/com/example/Starbucks/product/service/CategoryListServiceImpl.java
+++ src/main/java/com/example/Starbucks/product/service/CategoryListServiceImpl.java
@@ -8,10 +8,12 @@ import com.example.Starbucks.product.repository.MainCategoryRepository;
 import com.example.Starbucks.product.vo.RequestCategory;
 import com.example.Starbucks.product.vo.ResponseCategoryList;
 import com.example.Starbucks.product.vo.ResponseMainCategory;
+import com.example.Starbucks.product.vo.ResponsePage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +25,8 @@ import java.util.function.Function;
 @Slf4j
 @Service
 public class CategoryListServiceImpl implements ICategoryListService {
+
+    private final static int PAGE_SIZE = 3;
 
     private final MainCategoryRepository mainCategoryRepository;
     private final CategoryListRepository categoryListRepository;
@@ -54,15 +58,24 @@ public class CategoryListServiceImpl implements ICategoryListService {
     }
 
     @Override
-    public Page<ResponseCategoryList.categorySearchInfo> searchByCategory(Integer mainCategoryId, Integer middleCategoryId, Pageable pageable) {
+    public ResponsePage searchByCategory(Integer mainCategoryId, Integer middleCategoryId, Integer pageNum, Pageable pageable) {
+        pageable = PageRequest.of(pageNum, PAGE_SIZE);
         Page<CategoryList> categories = categoryListRepository.findAllByMainCategoryIdAndMiddleCategoryId(mainCategoryId, middleCategoryId, pageable);
-        return categoryListToSearchInfoMapper(categories);
+        return getPageInfo(categories);
     }
 
+//    @Override
+//    public Page<ResponseCategoryList.categorySearchInfo> searchByNameOrDescription(String keyword, String keyword2, Integer pageNum, Pageable pageable) {
+//        pageable = PageRequest.of(pageNum, PAGE_SIZE);
+//        Page<CategoryList> categories = categoryListRepository.findByProductNameContainingOrProductDescriptionContaining(keyword, keyword2, pageable);
+//        return categoryListToSearchInfoMapper(categories);
+//    }
+
     @Override
-    public Page<ResponseCategoryList.categorySearchInfo> searchByNameOrDescription(String keyword, String keyword2, Pageable pageable) {
+    public ResponsePage searchByNameOrDescription(String keyword, String keyword2, Integer pageNum, Pageable pageable) {
+        pageable = PageRequest.of(pageNum, PAGE_SIZE);
         Page<CategoryList> categories = categoryListRepository.findByProductNameContainingOrProductDescriptionContaining(keyword, keyword2, pageable);
-        return categoryListToSearchInfoMapper(categories);
+        return getPageInfo(categories);
     }
 
     public Page<ResponseCategoryList.categorySearchInfo> categoryListToSearchInfoMapper(Page<CategoryList> categories) {
@@ -81,6 +94,28 @@ public class CategoryListServiceImpl implements ICategoryListService {
             }
         });
         return responseSearchInfo;
+    }
+
+    public ResponsePage getPageInfo(Page<CategoryList> categories) {
+        List<ResponsePage.Contents> contents = new ArrayList<>();
+        for (CategoryList categoryList : categories.getContent()) {
+            Product product = categoryList.getProduct();
+            ResponsePage.Contents content = ResponsePage.Contents.builder()
+                    .productId(product.getId())
+                    .productName(product.getName())
+                    .price(product.getPrice())
+                    .thumbnail(product.getThumbnail())
+                    .build();
+            contents.add(content);
+        }
+        ResponsePage responsePage = ResponsePage.builder()
+                .pageSize(categories.getSize())
+                .pageNum(categories.getNumber())
+                .totalPage(categories.getTotalPages())
+                .totalElements(categories.getTotalElements())
+                .content(contents)
+                .build();
+        return responsePage;
     }
 
 
--- src/main/java/com/example/Starbucks/product/service/ICategoryListService.java
+++ src/main/java/com/example/Starbucks/product/service/ICategoryListService.java
@@ -3,6 +3,7 @@ package com.example.Starbucks.product.service;
 import com.example.Starbucks.product.model.MainCategory;
 import com.example.Starbucks.product.vo.RequestCategory;
 import com.example.Starbucks.product.vo.ResponseCategoryList;
+import com.example.Starbucks.product.vo.ResponsePage;
 import org.apache.coyote.Response;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,9 +18,11 @@ public interface ICategoryListService {
     //List<CategoryTypeDto> getAllType(String categoryType) ;
     void updateCategory(MainCategory mainCategory);
 
-    Page<ResponseCategoryList.categorySearchInfo> searchByCategory(Integer mainCategoryId, Integer middleCategoryInteger, Pageable pageable);
+    ResponsePage searchByCategory(Integer mainCategoryId, Integer middleCategoryInteger, Integer pageNum, Pageable pageable);
 
-    Page<ResponseCategoryList.categorySearchInfo> searchByNameOrDescription(String keyword, String keyword2, Pageable pageable);
+//    Page<ResponseCategoryList.categorySearchInfo> searchByNameOrDescription(String keyword, String keyword2, Integer pageNum, Pageable pageable);
+
+    ResponsePage searchByNameOrDescription(String keyword, String keyword2, Integer pageNum, Pageable pageable);
 
     //List<String> getCategoryTypeName();
 }
