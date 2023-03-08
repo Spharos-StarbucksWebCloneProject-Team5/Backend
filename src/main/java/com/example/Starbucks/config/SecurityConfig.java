4bac676294b0879c32898480201c928fed14dc97
KHS96 <7heesu7@hanmail.net>
2023-03-08 13:57:22 +0900

    Security

--- src/main/java/com/example/Starbucks/config/SecurityConfig.java
+++ src/main/java/com/example/Starbucks/config/SecurityConfig.java
@@ -1,52 +1,43 @@
 package com.example.Starbucks.config;
 
-//import com.example.Starbucks.jwt.JwtAuthenticationFilter;
-//import com.example.Starbucks.jwt.JwtTokenProvider;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-//    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
 
-    // authenticationManager를 Bean 등록합니다.
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .antMatchers("/api/v1/auth/**").permitAll()
+                .antMatchers("/api/v1/test/**").authenticated()
+                .antMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.csrf().disable();
-//        //http.httpBasic().disable(); // 일반적인 루트가 아닌 다른 방식으로 요청시 거절, header에 id, pw가 아닌 token(jwt)을 달고 간다. 그래서 basic이 아닌 bearer를 사용한다.
-        http.httpBasic().disable()
-                .authorizeRequests()// 요청에 대한 사용권한 체크
-//                .antMatchers("/test").authenticated()
-//                .antMatchers("/admin/**").hasRole("ADMIN")  //ADMIN권한이 요청 가능
-//                .antMatchers("/user/**").hasRole("USER")    //USER권한이 있어야 요청 가능
-                .antMatchers("*").permitAll();           //모두 허용
-//                .and()
-//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-//                        UsernamePasswordAuthenticationFilter.class); // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다
-//        // + 토큰에 저장된 유저정보를 활용하여야 하기 때문에 CustomUserDetailService 클래스를 생성합니다.
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // JWT를 사용하기 때문에 세션을 사용하지 않는다는 설정이다.
-
-
+        return http.build();
     }
 }
--- src/main/java/com/example/Starbucks/users/controller/TestController.java
+++ src/main/java/com/example/Starbucks/users/controller/TestController.java
@@ -1,17 +1,22 @@
 package com.example.Starbucks.users.controller;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
+@Slf4j
 public class TestController {
     @PostMapping("/test")
-    public String test(){
-        return "/test 통과";
+    public String test() {
+            return "/test 통과";
     }
 
-    @PostMapping("/admin/test")
+    @PostMapping("/admin/")
     public String test1(){
         return "admin 통과";
     }
