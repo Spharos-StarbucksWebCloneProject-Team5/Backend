508247a79646d6b7276e9115c3edd23934ef4cf6
KHS96 <7heesu7@hanmail.net>
2023-03-09 09:54:00 +0900

    security jwt redis

--- src/main/java/com/example/Starbucks/users/service/UserService.java
+++ src/main/java/com/example/Starbucks/users/service/UserService.java
@@ -1,13 +1,26 @@
 package com.example.Starbucks.users.service;
 
 //import com.example.Starbucks.users.dto.UserInfoDto;
+import com.example.Starbucks.users.dto.UserRequestDto;
 import com.example.Starbucks.users.model.User;
 import com.example.Starbucks.users.vo.RequestUser;
 import com.example.Starbucks.users.vo.ResponseUser;
 import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 
 public interface UserService {
-    ResponseUser addUser(RequestUser requestUser);
-    ResponseUser getUser(Long id);
+//    ResponseUser addUser(RequestUser requestUser);
+//    ResponseUser getUser(Long id);
+
+    public ResponseEntity<?> signUp(UserRequestDto.SignUp signUp);
+
+    public ResponseEntity<?> login(UserRequestDto.Login login);
+
+    public ResponseEntity<?> reissue(UserRequestDto.Reissue reissue);
+    public ResponseEntity<?> logout(UserRequestDto.Logout logout);
+
+    public ResponseEntity<?> authority();
+
+
 
 }
