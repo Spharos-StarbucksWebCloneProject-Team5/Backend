b7ece828c9baedac9ab2d663253e1c07dd361af0
jinyeong-afk <xoeh7002@naver.com>
2023-03-22 10:02:52 +0900

    feat: productAll paging 처리

--- src/main/java/com/example/Starbucks/product/controller/ProductController.java
+++ src/main/java/com/example/Starbucks/product/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.Starbucks.product.controller;
 
 import com.example.Starbucks.event.vo.RequestEvent;
+import com.example.Starbucks.product.dto.ResponseProductList;
 import com.example.Starbucks.product.model.Product;
 import com.example.Starbucks.product.service.IProductService;
 import com.example.Starbucks.product.vo.RequestProduct;
@@ -8,6 +9,8 @@ import com.example.Starbucks.product.vo.ResponseProduct;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,9 +36,9 @@ public class ProductController {
     }
 
     @Operation(summary = "상품 전체 조회 요청", description = "상품 데이터 전체를 조회합니다.", tags = {"상품"})
-    @GetMapping("all")
-    public ResponseEntity<List<ResponseProduct>> getAllProduct(){
-        return ResponseEntity.ok(iProductService.getAllProduct());
+    @GetMapping
+    public ResponseEntity<List<ResponseProductList>> getAllProduct(@Param("pageNum") int pageNum, Pageable pageable){
+        return ResponseEntity.ok(iProductService.getAllProduct(pageNum, pageable));
     }
 
     @Operation(summary = "상품 데이터 수정 요청", description = "요청한 상품 데이터 수정", tags = {"관리자"})
