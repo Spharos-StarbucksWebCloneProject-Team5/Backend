ed5eae6a3c0519c5b383d8563c5db11b0f4aeac2
jinyeong-afk <xoeh7002@naver.com>
2023-03-30 11:38:57 +0900

    feat(mainCategory): mainCategory 반환형 변경

--- src/main/java/com/example/Starbucks/category/service/MainCategoryService.java
+++ src/main/java/com/example/Starbucks/category/service/MainCategoryService.java
@@ -6,6 +6,7 @@ import com.example.Starbucks.category.dto.ResponseMainCategory;
 import java.util.List;
 
 public interface MainCategoryService {
+    List<ResponseMainCategory.Side> getAllSideMainCategories();
     List<ResponseMainCategory> getAllMainCategories();
     MainCategory addMainCategories(MainCategory mainCategory);
 
