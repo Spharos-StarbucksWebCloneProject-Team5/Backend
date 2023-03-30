bb7f9f670ab91e5921a9e7e098386c83f2c770c8
jinyeong-afk <xoeh7002@naver.com>
2023-03-30 20:58:58 +0900

    feat(jwt, user): 토큰 재발급 토큰값 헤더로 관리

--- src/main/java/com/example/Starbucks/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/Starbucks/jwt/JwtTokenProvider.java
@@ -25,8 +25,8 @@ import java.util.stream.Collectors;
 public class JwtTokenProvider {
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "Bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 30 * 2 *1000L;              // 30분
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 60 * 1000L;    // 7일
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 60 * 60 * 1000L;              // 1 시간
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 60 * 60 * 24 * 1000L;    // 1일
 
     private final Key key;
 
--- src/main/java/com/example/Starbucks/users/service/UserService.java
+++ src/main/java/com/example/Starbucks/users/service/UserService.java
@@ -19,7 +19,7 @@ public interface UserService {
 
     public ResponseEntity<?> login(UserRequestDto.Login login, HttpServletResponse httpServletResponse);
 
-    public ResponseEntity<?> reissue(UserRequestDto.Reissue reissue);
+    public ResponseEntity<?> reissue(String accessToken, String refreshToken, HttpServletResponse httpServletResponse);
     public ResponseEntity<?> logout(HttpServletRequest httpServletRequest);
 
     public ResponseEntity<?> authority();
