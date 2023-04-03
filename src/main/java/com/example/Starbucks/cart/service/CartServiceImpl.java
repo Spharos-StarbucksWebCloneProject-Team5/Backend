0289539283ba2c67664a86a4cfb0b134545e024f
tvfxq0214@gmail.com <tvfxq0214@gmail.com>
2023-04-03 19:24:55 +0900

    cart add ispresent modify

--- src/main/java/com/example/Starbucks/cart/service/CartServiceImpl.java
+++ src/main/java/com/example/Starbucks/cart/service/CartServiceImpl.java
@@ -13,6 +13,7 @@ import com.example.Starbucks.product.repository.IProductRepository;
 import com.example.Starbucks.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -33,21 +34,17 @@ public class CartServiceImpl implements ICartService{
 
     @Override
     public ResponseEntity<?> addCart(Authentication authentication, RequestCart requestCart) {
+        log.info(requestCart.toString());
         if(requestCart.getCount() > iProductRepository.findById(requestCart.getProductId()).get().getCount()){
             return responseInventory.cartFail(iProductRepository.findById(requestCart.getProductId()).get().getCount());
         }
         Long userId = userRepository.findByEmail(authentication.getName()).get().getId();
         //장바구니에 담았던 상품일 시 수량 추가
         Optional<Cart> cart = iCartRepository.findByUserIdAndProductId(userId, requestCart.getProductId());
-        log.info("{}",cart);
-        if(cart.isPresent()){
-            iCartRepository.save(Cart.builder()
-                    .id(cart.get().getId())
-                    .user(cart.get().getUser())
-                    .product(cart.get().getProduct())
-                    .count(cart.get().getCount() + requestCart.getCount())
-                    .now(true)
-                    .build());
+        log.info("{}",cart.isPresent());
+        if (cart.isPresent()){
+            cart.get().setUpdateCount(cart.get().getCount() + requestCart.getCount());
+            iCartRepository.save(cart.get());
             return responseInventory.cartSuccess();
         } else {
             iCartRepository.save(Cart.builder()
@@ -58,6 +55,7 @@ public class CartServiceImpl implements ICartService{
                     .build());
             return responseInventory.cartSuccess();
         }
+//        return null;
     }
 
     @Override
